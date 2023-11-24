9f817ae9e1d9b135c04c1fef30d12dc7b722ad17
Ivailo Aleksandrov <ivailoaleksandrov123@gmail.com>
2023-11-24 12:08:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -326,7 +326,7 @@ async function displaySlider() {
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
       </a>
       <h4 class="swiper-rating">
-        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.fixed(2)} / 10
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
       </h4>
     `;
 
